ad2c21dd8d1d96cffee76fb119b042597984f51a
IGOR COSTA DO COUTO <igor.couto@trt8.jus.br>
2025-03-28 10:56:56 +0000

    cars entity added in api

---
+++ api/src/main/java/com/tvdecontroller/models/entities/Cars.java
@@ -0,0 +1,14 @@
+package com.tvdecontroller.models.entities;
+
+import com.tvdecontroller.models.enums.FuelType;
+
+public class Cars {
+  public String brand;
+  public String model;
+  public String seats;
+  public FuelType fuel;
+  public String color;
+  public Double value;
+  public Integer monthOfFirstRegistration;
+  public Integer yearOfFirstRegistration;
+}
---
+++ api/src/main/java/com/tvdecontroller/models/enums/FuelType.java
@@ -0,0 +1,5 @@
+package com.tvdecontroller.models.enums;
+
+public enum FuelType {
+  ELETRIC, PETROL, DIESEL, NATURAL_GAS
+}
